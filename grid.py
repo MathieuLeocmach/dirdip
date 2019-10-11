1ad3e90510c8a1b069bace3943f6d884f14a4217
Mathieu Leocmach <mathieu.leocmach@univ-lyon1.fr>
2019-10-11 17:27:48 +0200

    Debug bin_weight_countdd, add the possibility to have no weights (call bin_count instead of bin_weight_count). Use bin_weight_countdd in the grid class, trimming away the coordinates outside of the grid. Tests, including several fields

--- grid.py
+++ grid.py
@@ -1,30 +1,55 @@
 import numpy as np
 from numba import jit
 
-#implementation using loops and numba
 @jit(nopython=True)
-def bin_weight_count(x, weights, minlength=0):
-    """Count number of occurrences of each value in array of non-negative ints. Also sums all weights for each value. If a value n is found at position i, count[n] += 1 and out[n] += weight[i].
+def bin_count(x, minlength=0):
+    """Count number of occurrences of each value in array of non-negative ints.
 
 The number of bins (of size 1) is one larger than the largest value in x. If minlength is specified, there will be at least this number of bins in the output array (though it will be longer if necessary, depending on the contents of x). Each bin gives the number of occurrences of its index value in x.
 
 Parameters
 ----------
 x : array_like, 1 dimension, nonnegative ints
-
     Input array.
     
-weights : array_like
-
-    Weights, array broadcastable with x (e.g. first dimension of weights is len(x)).
-    
 minlength : int
-
     A minimum number of bins for the output array.
 
 Returns
 ----------
-out : ndarray of the same type as weights
+count : 1D ndarray of ints
+    The number of elements of x in each bin. The length of count is `max(minlength, np.amax(x)+1)`.
+
+"""
+    mx = x.max()
+    mn = x.min()
+    assert mn >= 0
+    ans_size = max(mx+1, minlength)
+    ans = np.zeros((ans_size,), dtype=np.int64)
+    for i in x:
+        ans[i] = ans[i] + 1
+    return ans
+
+@jit(nopython=True)
+def bin_weight_count(x, weights, minlength=0):
+    """Count number of occurrences of each value in array of non-negative ints. Also sums all weights for each value. If a value n is found at position i, count[n] += 1 and sumw[n] += weight[i].
+
+The number of bins (of size 1) is one larger than the largest value in x. If minlength is specified, there will be at least this number of bins in the output array (though it will be longer if necessary, depending on the contents of x). Each bin gives the number of occurrences of its index value in x.
+
+Parameters
+----------
+x : array_like, 1 dimension, nonnegative ints
+    Input array.
+    
+weights : array_like
+    Weights, array broadcastable with x (e.g. first dimension of weights is len(x)).
+    
+minlength : int
+    A minimum number of bins for the output array.
+
+Returns
+----------
+sumw : ndarray of the same type as weights
     The sum of the weights in each bin. The shape of out is equal to `(max(minlength, np.amax(x)+1), wheights.shape[1:])`.
     
 count : 1D ndarray of ints
@@ -35,36 +60,33 @@ count : 1D ndarray of ints
     mn = x.min()
     assert mn >= 0
     ans_size = max(mx+1, minlength)
-    n = np.zeros((ans_size,), dtype=np.int64)
-    s = np.zeros((ans_size,) + weights.shape[1:], dtype=weights.dtype)
-    for i,w in zip(x, weights):
-        n[i] += 1
-        s[i] += w
-    return s,n
+    count = np.zeros((ans_size,), dtype=np.int64)
+    sumw = np.zeros((ans_size,) + weights.shape[1:], dtype=weights.dtype)
+    for p,i in enumerate(x):
+        count[i] += 1
+        sumw[i] += weights[p]
+    return sumw, count
     
-def bin_weight_countdd(ipos, weights, shape):
-    """Count number of occurrences of each D-uple of coordinates in array of non-negative integer coordinates. Also sums all weights for each value. If coordinate number p has indices (i,j,k), count[i,j,k] += 1 and out[i,j,k] += weight[p].
+def bin_weight_countdd(ipos, shape, weights=None):
+    """Count number of occurrences of each D-uple of coordinates in array of non-negative integer coordinates. Also sums all weights for each value. If coordinate number p has indices (i,j,k), count[i,j,k] += 1 and sumw[i,j,k] += weight[p].
 
 In any dimension d, no coordinate should be larger or equal than shape[d] or lower than 0. 
 
 Parameters
 ----------
-ipos : array_like, shape (P, D), nonnegative ints
-
-    Input array.
+ipos : array_like, shape (P, D), positive ints
+    Discretized coordinates on the grid.
     
-weights : array_like, shape (P, F)
-
-    Weights, array broadcastable with ipos (e.g. first dimension of weights is len(ipos)).
-    
-shape : sequence of int of size D
-
+shape : tuple of int of size D
     The multidimentional shape of the output array.
+    
+weights : array_like, shape (P, F), optional
+    Weights, array broadcastable with ipos (e.g. first dimension of weights is len(ipos)).
 
 Returns
 ----------
-out : ndarray of the same type as weights
-    The sum of the weights in each bin. The shape of out is equal to `(*shape, *wheights.shape[1:])`.
+sumw : ndarray of the same type as weights
+    The sum of the weights in each bin. The shape of out is equal to `(*shape, *wheights.shape[1:])`. If weights is None sumw is not returned.
     
 count : 1D ndarray of ints
     The number of elements of ipos in each bin. The length of count is `shape`.
@@ -72,54 +94,84 @@ count : 1D ndarray of ints
 """
     # Compute the sample indices in the flattened histogram matrix.
     # This raises an error if the array is too large.
-    xy = np.ravel_multi_index(ipos, shape)
+    xy = np.ravel_multi_index(ipos.T, shape)
+    if weights is None:
+        # Bin xy and assign it to the flattened histmat.
+        return bin_count(xy, minlength = np.prod(shape)).reshape(shape)
     # Bin xy (with and without weights) and assign it to the
     # flattened histmat.
-    s,c = np.bincount(xy, weights, minlength = np.prod(shape))
+    sumw, count = bin_weight_count(xy, weights, minlength = np.prod(shape))
     # Shape into a proper matrix
-    c = c.reshape(shape)
-    s = s.reshape(shape + c.shape[1:])
-    return s,n
+    count = count.reshape(shape)
+    sumw = sumw.reshape(shape + weights.shape[1:])
+    return sumw, count
 
 class Grid:
     """A class to manage D-dimensional rectilinear grids"""
-    def __init__(self, bins):
+    def __init__(self, edges):
         """Parameters
         ----------
         bins : A sequence of arrays describing the monotonically increasing bin
               edges along each dimension."""
-        self.bins = bins
+        for d, e in enumerate(edges):
+            if np.any(e[:-1] > e[1:]):
+                raise ValueError('`bins[{}]` must be monotonically increasing'.format(d))
+        self.edges = edges
         
     @property
-    def dim(self):
+    def ndim(self):
         """Dimensionality of the grid"""
-        return len(self.bins)
+        return len(self.edges)
+        
+    @property
+    def nbins(self):
+        """Shape of the array used for binning, margins included"""
+        return tuple(len(e)+1 for e in self.edges)
     
     def digitize(self, pos):
-        """snap positions to grid. 
-            Coordinates that are below the lowest bin will have index 0.
-            Coordinates that are above the highest bin will have index len(bin).
+        """snap positions to grid, assigning to it the index of the edge immediately larger to it.
+            Coordinates that are below the lowest edge will have index 0.
+            Coordinates that are above the highest edge will have index len(edges[d]).
         
         Parameters
         ----------
         pos : A (P,D) array of coordinates
         """
-        if pos.dim == 1:
-            return np.digitize(pos, self.bins[0])
-        return np.column_stack([np.digitize(x, b) for x,b in zip(pos.T, self.bins)])
+        if pos.ndim == 1:
+            return np.searchsorted(self.edges[0], pos, side='right')
+        return np.column_stack([
+            np.searchsorted(e, x, side='right')
+            for x,e in zip(pos.T, self.edges)
+            ])
+    
+    def count_sum_discreet(self, pos, field):
+        """sum per grid element the values of a scalar field defined only at coordinates pos"""
+        ipos = self.digitize(pos)
+        if pos.ndim == 1:
+            ipos = ipos[:,None]
+        sumw, count = bin_weight_countdd(ipos, self.nbins, weights=field)
+        #trim sides where the coordinates outside of the grid were binned
+        core = self.ndim*(slice(1, -1),)
+        return sumw[core], count[core]
     
     def count(self, pos):
-        """count how many points per grid element. Coordinates strictly below the lowest bin or strictly above the highest bin in any dimension are not counted."""
-        hist, bins = np.histogramdd(pos, self.bins)
-        return hist
+        """count how many points per grid element. Coordinates strictly below the lowest edge or above or equal to the highest edge in any dimension are not counted."""
+        ipos = self.digitize(pos)
+        if pos.ndim == 1:
+            ipos = ipos[:,None]
+        count = bin_weight_countdd(ipos, self.nbins)
+        #trim sides where the coordinates outside of the grid were binned
+        core = self.ndim*(slice(1, -1),)
+        return count[core]
     
     def sum_discreet(self, pos, field):
         """sum per grid element the values of a scalar field defined only at coordinates pos"""
-        hist, bins = np.histogramdd(pos, self.bins, weights=field)
-        return hist
+        sumw, count = self.count_sum_discreet(pos, field)
+        return sumw
     
     def mean_discreet(self, pos, field):
         """average per grid element the values of a scalar field defined only at coordinates pos"""
-        s = self.sum_discreet(pos, field)
-        n = self.count(pos)
-        return s/np.maximum(1, n)
+        sumw, count = self.count_sum_discreet(pos, field)
+        while count.ndim < sumw.ndim:
+            count = count[...,None]
+        return sumw/np.maximum(1, count)
--- tests.py
+++ tests.py
@@ -10,32 +10,46 @@ def test_bin_weight_count():
 
 def test_grid1D():
     grid = Grid([np.arange(0,10,2)])
-    assert_equal(grid.dim, 1)
+    assert_equal(grid.ndim, 1)
     xs = np.arange(-1,12)
-    assert_array_equal(grid.bins[0], np.arange(0,10,2))
-    assert_array_equal(grid.count(xs), [2,2,2,3])
+    assert_array_equal(grid.edges[0], np.arange(0,10,2))
+    assert_array_equal(grid.count(xs), [2,2,2,2])
     f = np.ones(xs.shape[0])
-    assert_array_equal(grid.sum_discreet(xs, f), [2,2,2,3])
+    assert_array_equal(grid.sum_discreet(xs, f), [2,2,2,2])
     assert_array_equal(grid.mean_discreet(xs, f), [1,1,1,1])
+    f2 = np.ones((xs.shape[0], 2))
+    assert_array_equal(grid.sum_discreet(xs, f2), np.full((4,2), 2))
+    assert_array_equal(grid.mean_discreet(xs, f2), np.ones((4,2)))
     
 def test_grid2D():
     grid = Grid([np.arange(0,10,2), np.arange(-3,12,3)])
-    assert_equal(grid.dim, 2)
+    assert_equal(grid.ndim, 2)
     xs = np.column_stack((np.arange(-1,12), np.arange(-1,12)))
-    assert_array_equal(grid.bins[0], np.arange(0,10,2))
+    assert_array_equal(grid.edges[0], np.arange(0,10,2))
     assert_array_equal(grid.count(xs), [
         [0, 2, 0, 0],
         [0, 1, 1, 0],
         [0, 0, 2, 0],
-        [0, 0, 0, 3]])
+        [0, 0, 0, 2]])
     f = np.ones(xs.shape[0])
     assert_array_equal(grid.sum_discreet(xs, f), [
         [0, 2, 0, 0],
         [0, 1, 1, 0],
         [0, 0, 2, 0],
-        [0, 0, 0, 3]])
+        [0, 0, 0, 2]])
     assert_array_equal(grid.mean_discreet(xs, f), [
         [0, 1, 0, 0],
         [0, 1, 1, 0],
         [0, 0, 1, 0],
         [0, 0, 0, 1]])
+    f2 = np.ones((xs.shape[0], 2))
+    assert_array_equal(grid.sum_discreet(xs, f2), np.dstack(2*[[
+        [0, 2, 0, 0],
+        [0, 1, 1, 0],
+        [0, 0, 2, 0],
+        [0, 0, 0, 2]]]))
+    assert_array_equal(grid.mean_discreet(xs, f2), np.dstack(2*[[
+        [0, 1, 0, 0],
+        [0, 1, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]]]))
